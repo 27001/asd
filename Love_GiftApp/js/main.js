536ba35f45199eaac57c1b6211d0b6686f237a6e
MzLove <tlove79@hotmail.com>
2013-12-04 19:14:13 -0500

    Gift App First Commit

---
+++ Love_GiftApp/js/main.js
@@ -0,0 +1,30 @@
+// Tai Love // ASD 1312
+
+
+// Home Page
+$("#home").on('pageinit', function () {
+
+}); // Home Page End
+
+
+
+// Gift Form Page
+$("#addGift").on('pageinit', function () {
+
+}); // Gift Form Page End
+
+
+
+// Interests and Ideas Page
+$("#addInfo").on('pageinit', function () {
+
+}); // Interests and Ideas Page End
+
+
+
+// Browse Page
+$("#browse").on('pageinit', function () {
+
+}); // Browse Page End
+
+
