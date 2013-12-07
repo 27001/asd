758699ab1776bbe48251f2827546bc569b5ac9a7
MzLove <tlove79@hotmail.com>
2013-12-07 12:39:36 -0500

    HTML pretty much done…Working on js

--- Love_GiftApp/js/main.js
+++ Love_GiftApp/js/main.js
@@ -1,6 +1,31 @@
 // Tai Love // ASD 1312
 
+var id;
+var parse = function (data) {};
+var items = [];
 
+// Get/Display Data
+var getTheData = function () {
+    if (localStorage.length === 0) {
+        alert("There is no data in Local Storage, so default data was added.");
+        autoPopulateData();
+    }
+    for (var i = 0, ls = localStorage.length; i < ls; i++) {
+        var key = localStorage.key(i);
+        var keyVal = localStorage.getItem(key);
+        var jsnObj = JSON.parse(keyVal);
+        for (var n in jsnObj) {
+            var newObj = jsnObj[n];
+            $('#browse').append(newObj + "<br />");
+        }
+        $('#browse').append(key + " " + '<br />');
+        $('#browse').append('<a href="#" class="edit" data-key="' + key[n] + '">Edit<a/> | <a href="#" data-key + key[n] + class="delete">Delete</a>');
+        $('.edit').attr('data-key', key[n]);
+    }
+}; // End Get Local Data
+
+        
+ /*       
 // Home Page
 $("#home").on('pageinit', function () {
 
@@ -17,17 +42,9 @@ $("#addGift").on('pageinit', function () {
 
 
 
-// Interests and Ideas Page
-$("#addInfo").on('pageinit', function () {
-
-}); // Interests and Ideas Page End
-
-
-
-
 // Browse Page
 $("#browse").on('pageinit', function () {
 
 }); // Browse Page End
 
-
+*/
